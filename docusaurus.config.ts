93abcfb8d615b7bc6a101890d550f8a8e0323785
Viren070 <viren070@protonmail.com>
2024-05-26 12:28:09 +0100

    add workflows

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,13 +11,14 @@ const config: Config = {
   url: 'https://viren070.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/guides/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'Viren070', // Usually your GitHub org/user name.
+  projectName: 'guides', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
