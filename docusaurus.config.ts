808755d80e9ef7bcfdd4ffef6c12a4a978afe3a5
Viren070 <viren070@protonmail.com>
2024-05-28 17:45:20 +0100

    update config for subdomain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://viren070.github.io/',
+  url: 'https://guides.viren070.me/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/guides/',
+  baseUrl: '',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
