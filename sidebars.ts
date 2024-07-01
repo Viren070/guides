13ebe4ce158ae761baec7ef612953edf6e418ab8
Viren070 <viren070@protonmail.com>
2024-07-01 17:45:34 +0100

    style: format sidebars.ts

--- sidebars.ts
+++ sidebars.ts
@@ -1,77 +1,96 @@
-import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
 const sidebars: SidebarsConfig = {
 	guides: [
-		'intro',
+		"intro",
 		{
-			type: 'category',
-			label: 'Games / Emulation',
+			type: "category",
+			label: "Games / Emulation",
 			link: {
-				type: 'generated-index', 
-				title: 'Games / Emulation', 
-				description: 'Guides on downloading and setting up games / emulators', 
-				keywords: ['games', 'free', 'download']
+				type: "generated-index",
+				title: "Games / Emulation",
+				description: "Guides on downloading and setting up games / emulators",
+				keywords: ["games", "free", "download"],
 			},
 			items: [
 				{
-					type: 'category',
-					label: 'Minecraft',
+					type: "category",
+					label: "Minecraft",
 					link: {
-						type: 'doc', 
-						id: 'minecraft/index' },
+						type: "doc",
+						id: "minecraft/index",
+					},
 					items: [
 						{
-							type: 'category',
-							label: 'Java Edition',
-							link: { type: 'doc', id: 'minecraft/java/index' },
-							items: []
+							type: "category",
+							label: "Java Edition",
+							link: { type: "doc", id: "minecraft/java/index" },
+							items: [],
 						},
 						{
-							type: 'category',
-							label: 'Bedrock Edition',
-							link: { type: 'doc', id: 'minecraft/bedrock/index' },
-							items: ['minecraft/bedrock/windows', 'minecraft/bedrock/android']
-						}
+							type: "category",
+							label: "Bedrock Edition",
+							link: { type: "doc", id: "minecraft/bedrock/index" },
+							items: ["minecraft/bedrock/windows", "minecraft/bedrock/android"],
+						},
 					],
 				},
 				{
-					type: 'category',
-					label: 'Plutonium',
-					link: { 
-						type: 'doc', 
-						id: 'plutonium/index' 
+					type: "category",
+					label: "Plutonium",
+					link: {
+						type: "doc",
+						id: "plutonium/index",
 					},
-					items: ['plutonium/intro', 'plutonium/setting-up-launcher', 'plutonium/downloading-base-game', 'plutonium/setting-up-on-plutonium', 'plutonium/credits']
-				}
-			]
+					items: [
+						"plutonium/intro",
+						"plutonium/setting-up-launcher",
+						"plutonium/downloading-base-game",
+						"plutonium/setting-up-on-plutonium",
+						"plutonium/credits",
+					],
+				},
+			],
 		},
 		{
-			type: 'category',
-			label: 'Movies / TV / Anime',
-			link: { 
-				type: 'generated-index', title: 'Movies / TV / Anime', description: 'Guides on using apps that can be used to stream movies / tv shows / anime', keywords: ['movies' , 'download'] 
+			type: "category",
+			label: "Movies / TV / Anime",
+			link: {
+				type: "generated-index",
+				title: "Movies / TV / Anime",
+				description:
+					"Guides on using apps that can be used to stream movies / tv shows / anime",
+				keywords: ["movies", "download"],
 			},
 			items: [
 				{
-					type: 'category',
-					label: 'Stremio',
-					link: { 
-						type: 'doc', id: 'stremio/index' 
+					type: "category",
+					label: "Stremio",
+					link: {
+						type: "doc",
+						id: "stremio/index",
 					},
 					items: [
-						'stremio/intro', 'stremio/technical-details', 'stremio/guide', 'stremio/extras', 'stremio/faq', 'stremio/troubleshooting', 'stremio/credits'
-					]
+						"stremio/intro",
+						"stremio/technical-details",
+						"stremio/guide",
+						"stremio/extras",
+						"stremio/faq",
+						"stremio/troubleshooting",
+						"stremio/credits",
+					],
 				},
 				{
-					type: 'category',
-					label: 'Kodi',
-					link: { 
-						type: 'doc', id: 'kodi/index' 
+					type: "category",
+					label: "Kodi",
+					link: {
+						type: "doc",
+						id: "kodi/index",
 					},
-					items: []
-				}
-			]
-		}
+					items: [],
+				},
+			],
+		},
 	],
 };
 
