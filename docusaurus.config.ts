fd0bcb794c44685304c9e0f817d47352b03672c5
Viren070 <viren070@protonmail.com>
2024-07-09 16:58:48 +0100

    feat: change title to Viren070's guides

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Guides',
+  title: 'Viren070\'s Guides',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
