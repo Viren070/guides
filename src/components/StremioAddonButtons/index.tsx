058a9d539e5fc298d49fd0117cd6cbc73386e453
Viren070 <viren070@protonmail.com>
2024-10-05 23:05:16 +0100

    move react components into components folder

---
+++ src/components/StremioAddonButtons/index.tsx
@@ -0,0 +1,66 @@
+import styles from './styles.module.css';
+
+interface StremioAddonButtonsProps {
+  source: string;
+  manifest: string;
+  configurable: boolean;
+  configurationRequired: boolean;
+  id: string;
+  configureAtBase: boolean;
+}
+
+export default function StremioAddonButtons(props: StremioAddonButtonsProps): JSX.Element {
+  const { source, manifest, configurable, configurationRequired, id, configureAtBase } = props;
+  const hasContent = source || manifest;
+
+  // Helper functions to modify URLs
+  const getConfigureUrl = (url: string) => configureAtBase ? url.replace(/manifest\.json$/, '') : url.replace(/manifest\.json$/, '') + 'configure';
+  const getInstallUrl = (url: string) => url.replace(/^https:\/\//, 'stremio://');
+  const getInstallWebUrl = (url: string) => `https://web.stremio.com/#/addons?addon=${url}`;
+
+  return (
+    <div>
+      {hasContent && (
+        <>
+          <div className={styles.buttonsContainer}>
+            {manifest && (
+              <div className={styles.buttonRow}>
+                {!configurationRequired && (  
+                  <>
+                    <button className={`${styles.button} ${styles.installButton}`}>
+                      <a href={getInstallUrl(manifest)}>➕ Install</a>
+                    </button>
+                    <button className={`${styles.button} ${styles.installWebButton}`}>
+                      <a href={getInstallWebUrl(manifest)}>🌐 Install (Web)</a>
+                    </button>
+                  </>
+                )}
+                {configurable && (
+                  <button className={`${styles.button} ${styles.configureButton}`}>
+                    <a href={getConfigureUrl(manifest)}>⚙️ Configure</a>
+                  </button>
+                )}
+              </div>
+            )}
+            {(source || id) && (
+              <div className={styles.buttonRow}>
+                {source && (
+                  <button className={`${styles.button} ${styles.sourceCodeButton}`}>
+                    <a href={source}>📝 Source Code</a>
+                  </button>
+                )}
+                {id && (
+                  <button className={`${styles.button} ${styles.guideLinkButton}`}>
+                    <a href={`addons?addon=${id}`} target="_blank" rel="noopener noreferrer">🔗 Addon Guide</a>
+                  </button>
+                )}
+              </div>
+            )}
+          </div>
+          <br />
+          <h3>Description</h3>
+        </>
+      )}
+    </div>
+  );
+}
