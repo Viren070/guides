2cc288b320de798746dbda9896bd480b06c87bff
Viren070 <viren070@protonmail.com>
2024-10-06 17:39:11 +0100

    feat: remove rel attribute for internal link

--- src/components/StremioAddonButtons/index.tsx
+++ src/components/StremioAddonButtons/index.tsx
@@ -51,7 +51,7 @@ export default function StremioAddonButtons(props: StremioAddonButtonsProps): JS
                 )}
                 {id && (
                   <button className={`${styles.button} ${styles.guideLinkButton}`}>
-                    <a href={`/stremio/addons/${id}`} target="_blank" rel="noopener noreferrer">🔗 Addon Guide</a>
+                    <a href={`/stremio/addons/${id}`} target="_blank">🔗 Addon Guide</a>
                   </button>
                 )}
               </div>
