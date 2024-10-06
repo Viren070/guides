65559d2900d1971347d13fe221b8ca33347cb32f
Viren070 <viren070@protonmail.com>
2024-10-06 16:46:22 +0100

    update locale to en-gb

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,8 +26,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'en-GB',
+    locales: ['en-GB'],
   },
 
   presets: [
