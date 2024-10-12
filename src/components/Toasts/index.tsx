3c4a41ac3a4bf504da585f6381fa83aff9b24378
Viren070 <viren070@protonmail.com>
2024-10-12 17:59:21 +0100

    remove style

--- src/components/Toasts/index.tsx
+++ src/components/Toasts/index.tsx
@@ -28,7 +28,7 @@ export const useToastSync = () => {
 
 export function MyToastContainer(): JSX.Element {
   return (
-    <ToastContainer stacked style={{width: "500px", padding: "1000px"}}/>
+    <ToastContainer stacked/>
   )
 }
 
