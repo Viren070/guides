6f379e9441554acfc9ab1de8194eae201e720cdf
Viren070 <viren070@protonmail.com>
2024-10-12 18:05:06 +0100

    remove leftover navbar item in config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,10 +70,6 @@ const config: Config = {
           widgetType: 'widget',
         },
         {
-          type: 'custom-toastcontainer',
-          position: 'right',
-        },
-        {
           href: 'https://github.com/Viren070/guides',
           position: 'right',
           className: 'header-github-link',
