d0851e0c89d16608c6e88aed25175a842da5aba8
Viren070 <viren070@protonmail.com>
2024-11-09 17:57:38 +0000

    feat(deps): enable experimental faster with lightningcss disabled

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,6 +26,16 @@ const config: Config = {
     locales: ['en-GB'],
   },
 
+  future: {
+    experimental_faster: {
+      swcJsLoader: true,
+      swcJsMinimizer: true,
+      swcHtmlMinimizer: true,
+      lightningCssMinimizer: false,
+      rspackBundler: true,
+      mdxCrossCompilerCache: true,
+    },
+  },
   presets: [
     [
       'classic',
