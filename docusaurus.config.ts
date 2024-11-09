7f355bd9c815df8cf6d80624901fbda622bb0e6a
Viren070 <viren070@protonmail.com>
2024-11-09 17:24:30 +0000

    Revert "Use @docusaurus/faster "

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,9 +6,7 @@ const config: Config = {
   title: 'Viren070\'s Guides',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
-  future: {
-    experimental_faster: true,
-  },
+
   // Set the production url of your site here
   url: 'https://guides.viren070.me/',
   // Set the /<baseUrl>/ pathname under which your site is served
