603047594f4c45f60225f3f6ca50b68d7b499ebc
Viren070 <viren070@protonmail.com>
2025-02-15 18:56:36 +0000

    feat(stremio): add easydebrid to service table

--- src/components/DebridCostComparisonTable/ServiceData.tsx
+++ src/components/DebridCostComparisonTable/ServiceData.tsx
@@ -6,6 +6,7 @@ export const initialServiceData = [
     { name: 'Debrid-Link', price: 25, duration: 300, currency: 'EUR' },
     { name: 'AllDebrid', price: 24.99, duration: 300, currency: 'EUR', pointData: { pointsPerPlan: 140, pointsRequiredForReward: 150, durationPerReward: 30 } },
     { name: 'Offcloud', price: 54.99, duration: 365, currency: 'USD' },
+    { name: 'EasyDebrid', price: 40, duration: 365, currency: 'USD' },
     { name: 'Premiumize', price: 69.99, duration: 365, currency: 'EUR' },
     { name: 'put.io (100GB)', price: 99, duration: 365, currency: 'USD' },
     { name: 'put.io (1TB)', price: 199, duration: 365, currency: 'USD' },
