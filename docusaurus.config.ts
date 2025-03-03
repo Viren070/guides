7a009dd8745a11d6e0a85fb7bee7f95980c632c5
Viren070 <viren070@protonmail.com>
2025-03-03 02:36:45 +0000

    feat: enable rspack bundler in docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,7 @@ const config: Config = {
       swcJsMinimizer: true,
       swcHtmlMinimizer: true,
       lightningCssMinimizer: true,
-      rspackBundler: false,
+      rspackBundler: true,
       mdxCrossCompilerCache: true,
     },
   },
